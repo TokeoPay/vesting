1d49f893a839e9dcdd14398de02f1077f085ce33
Gavin Harris <gavin@evie.digital>
2025-01-24 16:53:51 +1000

    Apply a patch to try fixing an issue selecting Collateral

--- toke-vesting/src/components/Available.tsx
+++ toke-vesting/src/components/Available.tsx
@@ -43,7 +43,7 @@ export function Available() {
   const tableButtonRef = useRef<HTMLButtonElement | null>(null);
   const { connected, wallet, name } = useWallet();
   const [addresses, setAddresses] = useState<string[]>();
-  const [cart, setCart] = useState<{[key: string]: VestingResponse}>({});
+  const [cart, setCart] = useState<{ [key: string]: VestingResponse }>({});
 
   const [total, setTotal] = useState(0);
   const [claimed, setClaimed] = useState(0);
@@ -57,7 +57,7 @@ export function Available() {
 
   const totalInCart = Object.values(cart).reduce(
     (acc, current) => acc + current.amount,
-    0
+    0,
   );
 
   useEffect(() => {
@@ -106,7 +106,7 @@ export function Available() {
       ([entry]) => {
         setIsButtonVisible(entry.isIntersecting);
       },
-      { threshold: 1.0 }
+      { threshold: 1.0 },
     );
 
     const tbr = tableButtonRef.current;
@@ -132,11 +132,11 @@ export function Available() {
       errorLogs.push("Initializing Lucid instance...");
       const lucid = await Lucid(
         new Kupmios("/kupo-mn", "/ogmios-mn"),
-        "Mainnet"
+        "Mainnet",
       );
       errorLogs.push("Selecting wallet from API...");
       lucid.selectWallet.fromAPI(
-        await window.cardano[name].enable()
+        await window.cardano[name].enable(),
         // wallet._walletInstance as unknown as WalletApi
       );
       errorLogs.push("Preparing Plutus script...");
@@ -148,24 +148,23 @@ export function Available() {
       const redeemer = Data.to(new Constr(0, []));
       // cart.values().map((utxo) => )
       errorLogs.push("Mapping UTxOs from cart...");
-      const scriptUtxos = Object.values(cart)
-        .map((utxo) => {
-          const assets: Record<string, bigint> = {};
+      const scriptUtxos = Object.values(cart).map((utxo) => {
+        const assets: Record<string, bigint> = {};
 
-          utxo.utxo.amount.forEach((asset) => {
-            assets[asset.unit] = BigInt(asset.quantity);
-          });
-
-          const u: UTxO = {
-            address: utxo.utxo.address,
-            outputIndex: utxo.utxo.output_index,
-            txHash: utxo.utxo.tx_hash,
-            assets: assets,
-            datum: utxo.utxo.inline_datum,
-          };
-
-          return u;
+        utxo.utxo.amount.forEach((asset) => {
+          assets[asset.unit] = BigInt(asset.quantity);
         });
+
+        const u: UTxO = {
+          address: utxo.utxo.address,
+          outputIndex: utxo.utxo.output_index,
+          txHash: utxo.utxo.tx_hash,
+          assets: assets,
+          datum: utxo.utxo.inline_datum,
+        };
+
+        return u;
+      });
 
       errorLogs.push("Building transaction...");
 
@@ -180,7 +179,7 @@ export function Available() {
       }
 
       const walletAddress = addressFromHexOrBech32(
-        (await wallet.getUsedAddresses())[0]
+        (await wallet.getUsedAddresses())[0],
       );
       errorLogs.push(`Wallet address obtained: ${walletAddress}`);
 
@@ -193,6 +192,7 @@ export function Available() {
           script: script.code,
         })
         .complete({
+          canonical: true,
           changeAddress: walletAddress.to_bech32(),
           localUPLCEval: false,
         });
@@ -207,17 +207,17 @@ export function Available() {
       lucid.awaitTx(txHash);
 
       toast(
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='#61D345'
-            className='size-8'
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#61D345"
+            className="size-8"
           >
             <path
-              fillRule='evenodd'
-              d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z'
-              clipRule='evenodd'
+              fillRule="evenodd"
+              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+              clipRule="evenodd"
             />
           </svg>
 
@@ -230,33 +230,33 @@ export function Available() {
             }}
           >
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke='currentColor'
-              className='size-4'
+              stroke="currentColor"
+              className="size-4"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
               />
             </svg>
           </button>
         </div>,
         {
           duration: 10000,
-        }
+        },
       );
 
-      const updatedCart: {[key: string]: VestingResponse} = {}
+      const updatedCart: { [key: string]: VestingResponse } = {};
 
       Object.entries(cart).forEach(([k, v]) => {
-        v.status = "Submitted"
-        updatedCart[k] = v
-      })
-     
+        v.status = "Submitted";
+        updatedCart[k] = v;
+      });
+
       setCart(updatedCart);
 
       // const confirmed = await lucid.awaitTx(txHash);
@@ -274,7 +274,7 @@ export function Available() {
       errorLogs.push(
         `Error encountered: ${
           e?.message || JSON.stringify(e) || "Unknown error"
-        }`
+        }`,
       );
       toast.error("Error processing cart");
       fetch("/api/log", {
@@ -345,46 +345,46 @@ export function Available() {
   }, [data]);
   // Sort data by vestingDate
   const sortedData = data?.sort(
-    (a: VestingResponse, b: VestingResponse) => a.vestingDate - b.vestingDate
+    (a: VestingResponse, b: VestingResponse) => a.vestingDate - b.vestingDate,
   );
 
   return (
-    <span className=' flex flex-col mt-4 md:mt-8 w-full h-full'>
-      <div className='w-full flex flex-col md:flex-row justify-between items-start md:items-end'>
-        <div className='flex flex-col'>
-          <p className='text-gray-600 font-semibold'>$TOKE</p>
-          <p className='text-gray-800 font-semibold text-2xl'>Token Vesting</p>
+    <span className=" flex flex-col mt-4 md:mt-8 w-full h-full">
+      <div className="w-full flex flex-col md:flex-row justify-between items-start md:items-end">
+        <div className="flex flex-col">
+          <p className="text-gray-600 font-semibold">$TOKE</p>
+          <p className="text-gray-800 font-semibold text-2xl">Token Vesting</p>
         </div>
-        <div className=' border border-gray-600 rounded-md  text-gray-800 flex items-center h-min overflow-clip '>
-          <p className='text-gray-800 p-1 border-r border-gray-600 text-sm bg-gray-200 font-semibold'>
+        <div className=" border border-gray-600 rounded-md  text-gray-800 flex items-center h-min overflow-clip ">
+          <p className="text-gray-800 p-1 border-r border-gray-600 text-sm bg-gray-200 font-semibold">
             $TOKE
           </p>
-          <div className='flex p-1 items-center'>
-            <p className='lg:flex hidden text-sm font-semibold'>
+          <div className="flex p-1 items-center">
+            <p className="lg:flex hidden text-sm font-semibold">
               375df3f2fb44d3c42b3381a09edd4ea2303a57ada32b5308c0774ee0
             </p>
-            <p className='lg:hidden flex text-sm'>375df3f2..8c0774ee0</p>
+            <p className="lg:hidden flex text-sm">375df3f2..8c0774ee0</p>
             <div
-              className='flex items-center pl-1'
+              className="flex items-center pl-1"
               onClick={() => {
                 navigator.clipboard.writeText(
-                  "375df3f2fb44d3c42b3381a09edd4ea2303a57ada32b5308c0774ee0"
+                  "375df3f2fb44d3c42b3381a09edd4ea2303a57ada32b5308c0774ee0",
                 );
                 toast.success("Copied to clipboard");
               }}
             >
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
                 strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-4'
+                stroke="currentColor"
+                className="size-4"
               >
                 <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z'
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
                 />
               </svg>
             </div>
@@ -392,20 +392,20 @@ export function Available() {
         </div>
       </div>
       {loadingState === "loading" ? (
-        <div className='w-full h-full flex justify-center items-center'>
-          <p className='text-gray-800 font-semibold'>Loading data...</p>
+        <div className="w-full h-full flex justify-center items-center">
+          <p className="text-gray-800 font-semibold">Loading data...</p>
         </div>
       ) : (
         <>
           {!connected ? (
-            <div className='max-w-max w-full mx-auto my-auto px-2 md:px-8 py-8 flex flex-col gap-6 items-center justify-center rounded-md border border-gray-600 bg-gray-200 shadow'>
+            <div className="max-w-max w-full mx-auto my-auto px-2 md:px-8 py-8 flex flex-col gap-6 items-center justify-center rounded-md border border-gray-600 bg-gray-200 shadow">
               <CardanoWallet />
-              <p className='text-gray-800 font-semibold text-lg'>
+              <p className="text-gray-800 font-semibold text-lg">
                 Connect your wallet to view your allocation
               </p>
             </div>
           ) : (
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <AllocationBar
                 total={total}
                 claimed={claimed}
@@ -415,17 +415,17 @@ export function Available() {
 
               <table
                 style={{ tableLayout: "fixed" }}
-                className='w-full border-collapse border border-gray-800 rounded-lg overflow-hidden shadow'
+                className="w-full border-collapse border border-gray-800 rounded-lg overflow-hidden shadow"
               >
                 <thead>
-                  <tr className='bg-gray-200'>
-                    <th className='px-4 py-3 text-gray-800 text-left font-semibold'>
+                  <tr className="bg-gray-200">
+                    <th className="px-4 py-3 text-gray-800 text-left font-semibold">
                       Vesting Date
                     </th>
-                    <th className='px-4 py-3 text-gray-800 text-left font-semibold'>
+                    <th className="px-4 py-3 text-gray-800 text-left font-semibold">
                       Token Amount
                     </th>
-                    <th className='px-4 py-3 text-gray-800 text-right font-semibold'>
+                    <th className="px-4 py-3 text-gray-800 text-right font-semibold">
                       Action
                     </th>
                   </tr>
@@ -442,20 +442,20 @@ export function Available() {
                           index % 2 === 0 ? "bg-white" : "bg-gray-100"
                         }`}
                       >
-                        <td className='px-4 py-2 text-gray-800'>
+                        <td className="px-4 py-2 text-gray-800">
                           {Intl.DateTimeFormat().format(
-                            new Date(u.vestingDate)
+                            new Date(u.vestingDate),
                           )}
                         </td>
-                        <td className='px-4 py-2 text-gray-800'>
+                        <td className="px-4 py-2 text-gray-800">
                           {u.amount.toLocaleString()} TOKE
                         </td>
-                        <td className='px-4 py-2 text-right'>
+                        <td className="px-4 py-2 text-right">
                           {u.status === "Submitted" ? (
                             <p
-                              className='
+                              className="
                               text-green-500 font-semibold py-1 px-4 rounded transition w-maz
-                            '
+                            "
                             >
                               Submitted
                             </p>
@@ -475,10 +475,10 @@ export function Available() {
                           ) : (
                             <button
                               disabled
-                              className='bg-gray-400 text-gray-600 font-semibold py-1 px-4 rounded cursor-not-allowed'
+                              className="bg-gray-400 text-gray-600 font-semibold py-1 px-4 rounded cursor-not-allowed"
                             >
-                              <p className='md:flex hidden'>Unavailable</p>
-                              <p className='md:hidden flex'>N/A</p>
+                              <p className="md:flex hidden">Unavailable</p>
+                              <p className="md:hidden flex">N/A</p>
                             </button>
                           )}
                         </td>
@@ -492,7 +492,7 @@ export function Available() {
                   <button
                     ref={tableButtonRef}
                     onClick={processCart}
-                    className='mt-2 bg-gray-800 hover:bg-gray-600 font-semibold text-white py-2 px-4 border rounded-md'
+                    className="mt-2 bg-gray-800 hover:bg-gray-600 font-semibold text-white py-2 px-4 border rounded-md"
                   >
                     {loadingState === "processing"
                       ? "Processing..."
@@ -501,10 +501,10 @@ export function Available() {
 
                   {/* Floating Button */}
                   {!isButtonVisible && (
-                    <div className='fixed bottom-0 left-0 right-0 bg-gray-800 p-4 shadow-lg z-50'>
+                    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 p-4 shadow-lg z-50">
                       <button
                         onClick={processCart}
-                        className='w-full font-semibold text-white py-3 px-4 rounded-md bg-blue-600 hover:bg-blue-500'
+                        className="w-full font-semibold text-white py-3 px-4 rounded-md bg-blue-600 hover:bg-blue-500"
                         disabled={loadingState === "processing"}
                       >
                         {loadingState === "processing"
@@ -539,12 +539,12 @@ export function AllocationBar({
   const lockedPercentage = (locked / total) * 100;
 
   return (
-    <div className='allocation-container mt-4' style={{ color: "#ffffff" }}>
-      <p className='font-semibold mb-2 text-gray-700 text-lg font'>
+    <div className="allocation-container mt-4" style={{ color: "#ffffff" }}>
+      <p className="font-semibold mb-2 text-gray-700 text-lg font">
         TOTAL ALLOCATION
       </p>
       <div
-        className='allocation-bar'
+        className="allocation-bar"
         style={{
           display: "flex",
           height: "8px",
@@ -575,7 +575,7 @@ export function AllocationBar({
           }}
         ></div>
       </div>
-      <div className='allocation-info mt-2 flex gap-x-4 w-full flex-wrap'>
+      <div className="allocation-info mt-2 flex gap-x-4 w-full flex-wrap">
         <span
           style={{ color: "#000000", display: "flex", alignItems: "center" }}
         >
