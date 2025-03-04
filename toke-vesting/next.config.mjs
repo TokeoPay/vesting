c77156db6e5e1c9dc6859195cfc62f96f306c317
Gavin Harris <gavin@evie.digital>
2025-03-04 11:26:23 +1000

    Add sourcemaps

--- toke-vesting/next.config.mjs
+++ toke-vesting/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   webpack: function (config, {}) {
     config.experiments = {
       asyncWebAssembly: true,
