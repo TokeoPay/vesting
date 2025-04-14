b0ad4f1706359056e240a303b45824d67e82339f
Gavin Harris <gavin@evie.digital>
2025-04-14 19:01:55 +1000

    Added an extra log message for when things go a little wrong

--- toke-vesting/src/components/Available.tsx
+++ toke-vesting/src/components/Available.tsx
@@ -15,7 +15,7 @@ import {
   addressFromHexOrBech32,
 } from "@lucid-evolution/lucid";
 
-import toast from "react-hot-toast";
+import toast, { ErrorIcon } from "react-hot-toast";
 
 export type VestingResponse = {
   utxo: {
@@ -178,6 +178,13 @@ export function Available() {
         await window.cardano[name].enable()
         // wallet._walletInstance as unknown as WalletApi
       );
+
+      const walletUtxos = await lucid.wallet().getUtxos();
+
+      if (walletUtxos.length == 0) {
+        throw new Error("No UTxOs in Wallet, please send your wallet some ADA");
+      }
+
       errorLogs.push("Preparing Plutus script...");
       const script: PlutusScript = {
         version: "V3",
@@ -315,7 +322,56 @@ export function Available() {
           e?.message || JSON.stringify(e) || "Unknown error"
         }`
       );
-      toast.error("Error processing cart");
+
+      if (e instanceof Error) {
+        toast.error(
+          <div className='flex items-center gap-2'>
+            <ErrorIcon></ErrorIcon>
+
+            <div />
+            <span className='flex flex-col'>
+              <p>Error processing cart</p>
+
+              {e?.message ? (
+                <p className='text-sm text-red-500'>{e.message}</p>
+              ) : null}
+
+              {errorLogs.map((e, i) => (
+                <p className='text-sm text-slate-500' key={i}>
+                  {e}
+                </p>
+              ))}
+            </span>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(JSON.stringify(errorLogs));
+                toast.success("Copied to clipboard");
+              }}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-4'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z'
+                />
+              </svg>
+            </button>
+          </div>,
+          {
+            duration: 350000,
+          }
+        );
+      } else {
+        toast.error("Error processing cart");
+      }
+
       fetch("/api/log", {
         method: "POST",
         headers: {
